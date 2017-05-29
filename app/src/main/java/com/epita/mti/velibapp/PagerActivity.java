99ab69431ebe5c792ca3a140b2e3bee546319cda
Alexandre <alexandre@realytics.io>
2017-05-29 22:19:29 +0200

    Add the tool bar for the pagerview

--- app/src/main/java/com/epita/mti/velibapp/PagerActivity.java
+++ app/src/main/java/com/epita/mti/velibapp/PagerActivity.java
@@ -2,8 +2,13 @@ package com.epita.mti.velibapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 /**
  * Created by alexa on 26/05/2017.
@@ -19,9 +24,38 @@ public class PagerActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_pager);
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        Toolbar listToolBar = (Toolbar) findViewById(R.id.pagerToolBar);
+        setSupportActionBar(listToolBar);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(fragmentManager);
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater menuInflater  = getMenuInflater();
+        menuInflater.inflate(R.menu.pager_toolbar_menu, menu);
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.pargermembers_list:
+                displayMembers();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void displayMembers()
